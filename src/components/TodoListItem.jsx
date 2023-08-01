85dc1751e5699d435b1d6d599dd12defe65d82b0
sik2 <devpang20@gmail.com>
2023-08-01 12:58:50 +0900

    1차 완성

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,6 +1,17 @@
-function TodoListItem({ todo }) {
-  console.log(todo.id);
-  return <div>{todo.text}</div>;
+function TodoListItem({ todo, onDelete, onToggle }) {
+  return (
+    <div style={{ textDecoration: todo.checked ? "line-through" : "none" }}>
+      <input
+        type="checkbox"
+        checked={todo.checked}
+        onChange={() => onToggle(todo.id)}
+      />
+      <span>
+        {todo.id}. {todo.text}
+      </span>
+      <button onClick={() => onDelete(todo.id)}>삭제</button>
+    </div>
+  );
 }
 
 export default TodoListItem;
